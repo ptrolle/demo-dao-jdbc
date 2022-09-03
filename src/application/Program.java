2b919ae66269cf8d2429fefdf9cddaf0ff4176fe
Pablo Trolle <pablotrolle@yahoo.com>
2022-09-03 16:00:38 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,11 +11,13 @@ public class Program {
 
     public static void main(String[] args){
 
-        Department obj = new Department(1, "Books");
+        /*Department obj = new Department(1, "Books");
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);*/
 
         SellerDao sellerDao = DaoFactory.createSellerDao(); //desta forma meu programa nao conhece a implementacao apenas a interface
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
