f574d07af14bef7771d722e731ed436857e7786b
Pablo Trolle <pablotrolle@yahoo.com>
2022-09-04 21:54:55 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -20,5 +21,12 @@ public class Program {
         Seller seller = sellerDao.findById(3); //para procurar o vendedor com o id 3 no banco de dados
 
         System.out.println(seller);
+
+        System.out.println("\n==== TEST 2: seller findByDepartment ====");
+        Department department = new Department(2, null );
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
