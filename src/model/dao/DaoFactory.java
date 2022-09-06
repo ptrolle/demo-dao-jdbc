d800718b313327facc69aed9e5f706f4c8a50f3e
Pablo Trolle <pablotrolle@yahoo.com>
2022-09-06 11:37:35 -0300

    Done

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 //para instanciar os objetos Seller
@@ -8,5 +9,9 @@ public class DaoFactory {
 
     public static SellerDao createSellerDao(){
        return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
